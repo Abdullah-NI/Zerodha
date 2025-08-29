86c269ca7b8efd1fc0cdb6dc1b4ab5a49df8e9df
Abdullah <abdullahnidbd786@gmail.com>
2025-08-29 10:09:14 +0530

    cookiess

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -17,7 +17,7 @@ module.exports.Signup = async (req, res, next) => {
       httpOnly: true, 
       secure: process.env.NODE_ENV === "production",
       sameSite: "None",
-      maxAge: 24 * 60 * 60 * 1000 // 1 din
+      // maxAge: 24 * 60 * 60 * 1000 // 1 din
     }); 
     res
       .status(201)
